a7304716fa9325b9f0c3e9c0540af265e5e44d65
S-GADIOUX <sebgad19@gmail.com>
2019-04-02 17:47:19 +0200

    LF earley done

--- LF/gadioux_earley_3.py
+++ LF/gadioux_earley_3.py
@@ -100,13 +100,15 @@ class Item:
 	# field lhs: Symbol
 	# field bd: list of Symbol
 	# field ad: list of Symbol
+	# field tree: class Tree usefull for the syntax tree
 	
-	def __init__(self, i, j, lhs, bd, ad): # [lhs --> bd . ad, i, j]
+	def __init__(self, i, j, lhs, bd, ad, tree = None): # [lhs --> bd . ad, i, j]
 		self.i = i
 		self.j = j
 		self.lhs = lhs
 		self.bd = bd
 		self.ad = ad
+		self.tree = tree
 		
 	def __str__(self):
 		return "[%d, %d, %s --> %s * %s]" % \
@@ -115,6 +117,9 @@ class Item:
 	def __eq__(self, other):
 		return self.i == other.i and self.j == other.j and \
 			self.lhs == other.lhs and self.bd == other.bd and self.ad == other.ad
+
+	def __hash__(self):
+		return str(self).__hash__()
 			
 class ParseChart:
 	# field ch: list of Item
@@ -125,27 +130,56 @@ class ParseChart:
 	ch = [] # Chart
 	ag = [] # Agenda
 	
-	def __init__(self):
+	def __init__(self, verbose = False):
 		self.ch = []
+		self.chset = set()
 		self.ag = []
+		self.agset = set() # Because thank you recursives stupids rules...
+		self.verbose = verbose
 		
 	# Adds an item at the end of the agenda (+ prints some log)
 	def agAppend(self, item, reason = None):
-		self.ag.append(item)
-		
-		if reason != None:
-			print( str(item) + " appended to the agenda (" + reason + ")")
-		else:
-			print( str(item) + " appended to the agenda")
+		if item not in self.agset :
+			self.ag.append(item)
+			self.agset.add(item)
+			if reason != None:
+				print( str(item) + " appended to the agenda (" + reason + ")")
+			else:
+				print( str(item) + " appended to the agenda")
+		else :
+			print( str(item) + " already in agenda ")
+
 		
 	# Adds an item at the end of the chart (+ prints some log)
 	def chAppend(self, item, reason = None):
-		self.ch.append(item)
-		
-		if reason != None:
-			print( str(item) + " appended to the chart (" + reason + ")" )
+		if item not in self.chset :
+			self.ch.append(item)
+			self.chset.add(item)
+			if reason != None:
+				print( str(item) + " appended to the chart (" + reason + ")")
+			else:
+				print( str(item) + " appended to the chart")
+			return True
+		else :
+			print( str(item) + " already in chart !!!!!!!!!!!!!!")
+			return False
+
+class Tree:
+	# field branches: liste de longueur 1 ou 2 (seules possibilités avec une grammaire en FNC).
+	# Si la longueur est 1, l'element est une liste de 1 symbole.
+	# Si la longueur est 2, les éléments sont eux-mêmes des arbres.
+	# field label: Symbol
+	# (no methods)
+
+	def __init__(self, label, branches):
+			self.branches = branches
+			self.label = label
+	
+	def __str__(self):
+		if len(self.branches) == 1:
+			return self.label.name + "\n  " + (str(self.branches[0]).replace("\n", "\n  "))
 		else:
-			print( str(item) + " appended to the chart" )
+			return self.label.name + "\n  " + str(self.branches[0]).replace("\n", "\n  ") + "\n  " + str(self.branches[1]).replace("\n", "\n  ")
 
 # ------------------------
 
@@ -157,6 +191,8 @@ def init(g, w, t):
 	for rule in g.rules :
 		if rule.lhs == g.axiom :	# Ajout des règles axiomatiques.
 			t.agAppend( Item(0, 0, g.axiom, [], rule.rhs ), "Init" )
+			if t.verbose :
+				input()
 
 # Insère dans l'agenda les éventuels nouveaux items issus de la règle pred ou scan pour l'item it
 def pred_scan(g, w, it, t):
@@ -168,12 +204,14 @@ def pred_scan(g, w, it, t):
 		#Pred
 		if g.isNonTerminal(it.ad[0]) :
 			for rule in g.rules :	# Recherche des règles pouvant être produite à partir de la tête de la partie encore active de l'item.
-				if rule.lhs.name == it.ad[0].name:
+				if rule.lhs.name == it.ad[0].name and not ( len(rule.rhs) == 1 and rule.lhs.name == rule.rhs[0].name ):
 					t.agAppend( Item(it.j, it.j, rule.lhs, [], rule.rhs), "Pred")
 		#Scan
 		else :
-			if w[it.i].name == it.ad[0].name :	# Comparaison de la tête de lecture avec le terminal dans la règle.
-				t.agAppend( Item(it.j, it.j+1, it.lhs, it.bd + [it.ad[0]], it.ad[1:]), "Scan")
+			if it.j < len(w) and w[it.j].name == it.ad[0].name :	# Comparaison de la tête de lecture avec le terminal dans la règle.
+				t.agAppend( Item(it.j, it.j+1, it.lhs, it.bd + [it.ad[0]], it.ad[1:], tree = Tree( it.lhs ,[w[it.i]]) ), "Scan") # Ajout egalement de l'arbre syntaxique produit par la regle.
+	if t.verbose :
+		input()
 
 # Insère dans l'agenda les éventuels nouveaux items issus de la règle comp pour l'item it
 def comp(g, w, it, t):
@@ -188,13 +226,16 @@ def comp(g, w, it, t):
 				b += comp_help(it, item, t)
 			elif not it.ad and item.ad : # Si l'autre item est actif
 				b += comp_help(item, it, t)
+	if t.verbose :
+		input()
 	return b
 
 def comp_help(act, inact, t):
 	# Fonction de support de comp qui prend en entrée deux item, un actif et l'autre inactif ainsi qu'une table T.
 	# La fonction tente ensuite une compilation.
 	if act.j == inact.i and act.ad[0].name == inact.lhs.name :
-		t.agAppend( Item(act.i, inact.j, act.lhs, act.bd + [act.ad[0]], act.ad[1:]), "Comp")
+		t.agAppend( Item(act.i, inact.j, act.lhs, act.bd + [act.ad[0]], act.ad[1:], tree = Tree(inact.lhs, [act.tree, inact.tree] ) if act.tree else inact.tree ), "Comp")
+		#les arbres sont egalement gérés au dessus.
 		return True
 	return False
 
@@ -203,33 +244,37 @@ def table_complete(g, w, t):
 	# g: Grammar
 	# w: list of Symbol
 	# t: ParseChart
-	
+	axioms = []
 	for item in t.ch : # Verifie si l'item est l'axiome, qu'il a bien lu tout le mot et que la regle est inactive.
 		if item.lhs.name == g.axiom.name and not item.i and item.j == len(w) and not item.ad :
-			return True
+			axioms.append(item)
 	
-	return False
+	return axioms
 
 # Parse le mot w pour la grammaire g ; retourne le chart à la fin de l'algorithme
-def parse_earley(g, w):
+def parse_earley(g, w, v):
 	# g: Grammar
 	# w: list of Symbol
 	
-	T = ParseChart()
+	T = ParseChart(v)
 	
 	# Initialisation
 	init(g, w, T)
 	
 	# Boucle sur l'agenda : on sort un item, on l'insère dans la chart, et toutes ses conséquences sont insérées dans l'agenda
 	while T.ag != []:
-		print("")
+		if not v :
+			print("")
 		it = T.ag.pop(0)	# Sortie de l'agenda
-		T.chAppend(it)		# Ajout dans la table
-		if not comp(g, w, it, T) :	#Calcul de comp
-			pred_scan(g, w, it ,T)	# Calcul de pred et scan
-		
-	if table_complete(g, w, T):
+		T.agset.remove(it)
+		if T.chAppend(it) :		# Ajout dans la table
+			if not comp(g, w, it, T) :	#Calcul de comp
+				pred_scan(g, w, it ,T)	# Calcul de pred et scan
+	trees = table_complete(g, w, T)
+	if len(trees):
 		print( "\n****Parsing réussi****" )
+		for tree in trees :
+			print("Tree :\n" + str(tree.tree))
 	else:
 		print( "\n****Parsing échoué****" )
 	
@@ -243,6 +288,7 @@ symS = Symbol("S")
 symA = Symbol("A")
 symTerminalA = Symbol("a")
 symTerminalB = Symbol("b")
+symEpsilon = Symbol("")
 
 # Definition of a grammar
 g1 = Grammar(
@@ -254,7 +300,7 @@ g1 = Grammar(
 	
 	# List of rules
 	[
-		Rule(symS, [symA, symS]), 				# S --> AS
+		Rule(symS, [symA, symS]), 					# S --> AS
 		Rule(symS, [symTerminalB]), 				# S --> b
 		Rule(symA, [symTerminalA]), 				# A --> a
 	],
@@ -263,7 +309,48 @@ g1 = Grammar(
 	"g1"
 )
 
-print( g1 )
+g2 = Grammar(
+	# All symbols
+	[symS, symA, symTerminalA, symTerminalB],
+	
+	# Axiom
+	symS,
+	
+	# List of rules
+	[
+		Rule(symS, [symA, symS]), 		# S --> AS
+		Rule(symS, [symTerminalB]), 	# S --> b
+		Rule(symA, [symTerminalA]), 	# A --> a
+		Rule(symA, [symA]), 			# A --> A
+	],
+	
+	# name
+	"g2"
+	)
+
+g3 = Grammar(
+	# All symbols
+	[symS, symA, symTerminalA, symTerminalB, symEpsilon],
+	
+	# Axiom
+	symS,
+	
+	# List of rules
+	[
+		Rule(symS, [symA, symS]), 					# S --> AS
+		Rule(symS, [symA]), 						# S --> A
+		Rule(symS, [symTerminalB]), 				# S --> b
+		Rule(symS, [symTerminalB, symTerminalB]), 	# S --> bb
+		Rule(symA, [symTerminalA]), 				# A --> a
+		Rule(symA, [symS]), 						# A --> S
+		Rule(symA, [symEpsilon]),					# A --> e
+	],
+	
+	# name
+	"g3"
+	)
+
+print( g2 )
 
 words = ["aab", "b", "aaaaab", "abab"]
 #words = ["aab"]
@@ -293,7 +380,7 @@ def wordToTerminals(w, g):
 for w in words:
 	print( "\n#### Mot : " + w + " ####")
 	
-	chart = parse_earley(g1, wordToTerminals(w, g1))
+	chart = parse_earley(g1, wordToTerminals(w, g3), False)
 	
 	print( "chart:" )
 	for item in chart:
