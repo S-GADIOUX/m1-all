411f0f18e51c65238b30b83e56bc81e6911a174e
Sebastien <sebastien@exensa.com>
2019-05-21 13:41:58 +0200

    Vector creation done

--- Projet/semi-supervised_WSD/src/Reader.py
+++ Projet/semi-supervised_WSD/src/Reader.py
@@ -1,26 +1,62 @@
 # coding: utf-8
 import pprint as pp
 import pandas as pd
+from itertools import chain
 
-def sentence2vec(listOfConllLines, key, embeddings, left, right) :
-	#index, surf, lemme, simpCat, detCat, _, linkIndex, linkRel = 
+class cat_dict :
+
+	def __init__(self):
+		self.dict = {"BEFORE" : 0, "AFTER" : 1}
+
+	def add_cat(self, cat):
+		if cat.upper() not in self.dict :
+			self.dict[cat.upper()] = len(self.dict)
+
+	def vector_of_cat(self, cat) :
+		vec = [0 for i in range(len(self.dict))]
+		vec[self.dict[cat.upper()]] = 1
+		return vec
+
+def embedding_reader(file):
+	embeddings = {}
+	with open(file, encoding = "utf-8" ) as stream :
+		nb, dim = [ int(s) for s in stream.readline()[:-1].split(" ") ]
+		sums = [0 for i in range(dim)]
+		for i in range(nb) :
+			datas = stream.readline()[:-1].split(" ")
+			vec = [float(s) for s in datas[1:]]
+			embeddings[datas[0]] = vec
+			sums = [sums[i]+vec[i] for i in range(dim)]
+			stream.readline()
+		sums = [sums[i]/nb for i in range(dim)]
+		embeddings["#MEDIAN#"] = sums
+	return embeddings
+
+def sentence2vec(list_of_conll_lines, key, embeddings, left, right, cat_dictio) :
 	datas = pd.DataFrame(
 			columns = ["index", "surf", "lemme", "simpCat", "detCat",
 						"Useless", "linkIndex", "linkRel"])
-	for word in listOfConllLines :
-		print(pd.Series( word, index = datas.columns ))
+	for word in list_of_conll_lines :
 		datas = datas.append(pd.Series( word, index = datas.columns ), ignore_index = True, )
 	datas.drop(columns = ['Useless'], inplace = True)
 	datas.set_index('index', inplace = True)
 	datas["linkIndex"] = [int(s.split("|")[0]) for s in datas["linkIndex"]]
 	datas["linkRel"] = [s.split("|")[0] for s in datas["linkRel"]]
-	print(datas)
-	input()
+	for cat in datas["detCat"] :
+		cat_dictio.add_cat(cat)
+	key_index = int(datas["lemme"][datas["lemme"] == key].index[0])
+	vector = []
+	cats = []
+	for i in chain(range(max(1, key_index-left), key_index),
+					range(key_index+1, min(len(list_of_conll_lines), key_index+right)+1)) :
+		vector+= embeddings.get(datas["lemme"].at[str(i)], embeddings["#MEDIAN#"])
+		cats += [datas["detCat"].at[str(i)]]
 		
-	pass 
+	return (vector,cats)
 
-def file2vec(file, embeddings, key):
-	vecs = []
+def file2vec(file, key, embeddings, left, right):
+	vec_and_cats = []
+	cat_dictio = cat_dict()
 	with open(file, encoding = "utf-8") as stream :
 		line = stream.readline()
 		current = []
@@ -28,21 +64,30 @@ def file2vec(file, embeddings, key):
 			if line[:-1]:
 				current.append(line[:-1].split("\t")[:-2])
 			else :
-				vecs.append(sentence2vec(current, key, embeddings,2, 3))
+				vec_and_cats.append(sentence2vec(current, key, embeddings, left, right, cat_dictio))
 				current = []
-
 			line = stream.readline()
+	
+	vector_list = []
+	for vec, cats in vec_and_cats :
+		for cat in cats :
+			vec+= cat_dictio.vector_of_cat(cat)
+		vector_list.append(vec)
+	return vector_list
 
-def embeddingReader(file):
-	embeddings = {}
-	with open(file, encoding = "utf-8" ) as stream :
-		nb, dim = [ int(s) for s in stream.readline()[:-1].split(" ") ]
-		for i in range(nb) :
-			datas = stream.readline()[:-1].split(" ")
-			embeddings[datas[0]] = [float(s) for s in datas[1:]]
-	return embeddings
+def merge_with_gold_file(vectors, gold_file):
+	vec_and_gold = []
+	with open(gold_file, encoding = "utf-8") as stream :
+		for vector in vectors :
+			gold = int(stream.readline()[:-1])
+			vec_and_gold.append((vector, gold))
+	return vec_and_gold
 
-embeddings = embeddingReader("../data/w2v_final.txt")
+		
 
-file2vec("../data/compter/compter-150.conll", "compter", embeddings)
+embeddings = embedding_reader("../data/w2v_final.txt")
 
+vecs = file2vec("../data/compter/compter-150.conll", "compter", embeddings, 2, 3)
+
+datas = merge_with_gold_file(vecs,"../data/compter/compter-150.gold")
+pp.pprint(datas)
