eac6ddd3d0195e2700c8e7aaa32d9ff6233b8531
S-GADIOUX <sebgad19@gmail.com>
2019-06-16 16:26:33 +0200

    Reader Done

--- Projet/semi-supervised_WSD/src/Reader.py
+++ Projet/semi-supervised_WSD/src/Reader.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 import pprint as pp
 import pandas as pd
+import numpy as np
 from itertools import chain
+import pickle
 
 class cat_dict :
 	#Class to create one hot vector of categories.
@@ -55,7 +57,7 @@ def embedding_reader(file):
 	
 	return embeddings
 
-def sentence2vec(list_of_conll_lines, key, embeddings, left, right, cat_dictio) :
+def sentence2vec(list_of_conll_lines, key, embeddings, left, right, cat_dictio, syntax) :
 	#Turn a sentence to a vector.
 	#input list_of_conll_lines : list[] / list of words and meta datas in a sentence
 	#input key : string / the word we want to classify
@@ -70,7 +72,7 @@ def sentence2vec(list_of_conll_lines, key, embeddings, left, right, cat_dictio) 
 						"Useless", "linkIndex", "linkRel"])
 	#Add all words and meta datas in the data frame
 	for word in list_of_conll_lines :
-		datas = datas.append(pd.Series( word, index = datas.columns ), ignore_index = True, )
+		datas = datas.append(pd.Series( word, index = datas.columns ), ignore_index = True)
 	#Clean the dataframe.
 	datas.drop(columns = ['Useless'], inplace = True)
 	datas.set_index('index', inplace = True)
@@ -83,48 +85,63 @@ def sentence2vec(list_of_conll_lines, key, embeddings, left, right, cat_dictio) 
 	
 	#Find the index of the word to classify
 	key_index = int(datas["lemme"][datas["lemme"] == key].index[0])
-	
-
 	vector = []
 	cats = []
+	
 	#Add the median vector when the word is near the start of the sentence
 	if key_index-left < 1:
 		for i in range(key_index-left,1):
-			vector += embeddings["#MEDIAN#"]
+			vector.append(embeddings["#MEDIAN#"])
 			cats += ["BEFORE"]
+	
 	#Add all vectors inside the sentence
 	for i in chain(range(max(1, key_index-left), key_index),
 					range(key_index+1, min(len(list_of_conll_lines), key_index+right)+1)) :
-		vector+= embeddings.get(datas["lemme"].at[str(i)], embeddings["#MEDIAN#"])
+		vector.append(embeddings.get(datas["lemme"].at[str(i)],
+								embeddings["#MEDIAN#"]))
 		cats += [datas["detCat"].at[str(i)]]
+	
 	#Add the median vector when the word is near the end of the sentence
 	if len(list_of_conll_lines) < key_index+right:
 		for i in range(len(list_of_conll_lines)+1,key_index+right+1):
-			vector += embeddings["#MEDIAN#"]
+			vector.append(embeddings["#MEDIAN#"])
 			cats += ["AFTER"]
+	
+	if syntax :
+		linked = datas["linkRel"][datas["linkIndex"] == key_index]
+		print(linked)
+		for func in ["suj:suj", "obj:obj"] :
+			maybe = linked[linked == func].index
+			if len(maybe) :	
+				vector.append(embeddings.get(datas["lemme"].at[str(maybe[0])],
+										embeddings["#MEDIAN#"]))
+				cats += [datas["detCat"].at[str(maybe[0])]]
+		print([x[1] for x in list_of_conll_lines])
+		input()
+		
 	return (vector,cats)
 
-def file2vec(file, key, embeddings, left, right) :
+def file2vec(file, key, embeddings, left, right, syntax) :
 	#Turn a file into a list of vector
 	vec_and_cats = []
 	cat_dictio = cat_dict()
 	with open(file, encoding = "utf-8") as stream :
 		line = stream.readline()
-		current = []
+		current_line = []
 		while line :
 			if line[:-1]:
-				current.append(line[:-1].split("\t")[:-2])
+				current_line.append(line[:-1].split("\t")[:-2])
 			else :
-				vec_and_cats.append(sentence2vec(current, key, embeddings, left, right, cat_dictio))
-				current = []
+				vec_and_cats.append(sentence2vec(current_line, key, embeddings, left, right, cat_dictio, syntax))
+				current_line = []
 			line = stream.readline()
-	
+
 	vector_list = []
 	for vec, cats in vec_and_cats :
-		for cat in cats :
-			vec+= cat_dictio.vector_of_cat(cat)
-		vector_list.append(vec)
-	print(embeddings["#MEDIAN#"])
+		final_vector = []
+		for emb, oh in zip(vec,cats) :
+			final_vector += emb + cat_dictio.vector_of_cat(cat)
+		vector_list.append(np.array(final_vector))
 	return vector_list
 
 def merge_with_gold_file(vectors, gold_file):
@@ -137,13 +154,21 @@ def merge_with_gold_file(vectors, gold_file):
 
 
 
-embeddings = embedding_reader("../data/w2v_final.txt")
-print("embeddings")
-vecs = file2vec("../data/compter/compter-150.conll", "compter", embeddings, 2, 3)
-print("vecs")
+top_folder = "../data/"
+target = "abattre"
+nb = "161"
 
-datas = merge_with_gold_file(vecs,"../data/compter/compter-150.gold")
-import pickle
-pickle.dump(datas, open('compter.pkl', 'wb'))
-with open('compter.pkl', 'rb') as f1:
+def generate(top_folder, target, nb, left, right, syntax):
+	embeddings = embedding_reader(top_folder+"w2v_final.txt")
+	print("Embeddings loaded")
+	vecs = file2vec(top_folder+target+"/"+target+"-"+nb+".conll", target, embeddings, left, right, syntax)
+	print("Vectors ready")
+
+	datas = merge_with_gold_file(vecs, top_folder+target+"/"+target+"-"+nb+".gold")
+	pickle.dump(datas, open(target+'pkl', 'wb'))
+
+generate(top_folder, target, nb, 2, 3, True)
+"""
+with open(target+'pkl', 'rb') as f1:
 	datas = f1.read()
+"""
